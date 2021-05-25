4b54cca46038f8c5ee71d121daa23563d86dd961
my1124093 <tianlong369990@163.com>
2021-05-26 03:43:30 +0800

    redis

--- ext.py
+++ ext.py
@@ -13,3 +13,29 @@ def get_all_info():
     for key in r.scan_iter():
         print key, r.get(key)
         res.append({key: r.get(key)})
+
+def create_db():
+    value0 = {
+        'device_id': None,
+        'time': None,
+        'filename': 'a.jpeg'
+    }
+    r.set(0, value0)
+    value1 = {
+        'device_id': None,
+        'time': None,
+        'filename': 'b.jpeg'
+    }
+    r.set(1, value1)
+    value2 = {
+        'device_id': None,
+        'time': None,
+        'filename': 'c.jpeg'
+    }
+    r.set(2, value2)
+    value3 = {
+        'device_id': None,
+        'time': None,
+        'filename': 'd.jpeg'
+    }
+    r.set(3, value3)
